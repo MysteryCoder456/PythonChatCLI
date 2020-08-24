7ea772d44e5793bd471284bc10ae1f097fc4ab00
MysteryCoder456 <rehatbir.singh@gmail.com>
2020-08-25 02:29:48 +0400

    added ability to send messages from clien to server

--- client.py
+++ client.py
@@ -1,11 +1,58 @@
+import sys
 import socket
+import threading
+
+IP = '127.0.0.1'
+PORT = 8000
+MSG_SIZE = 2048
 
 usern = input("Enter your username: ")
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('127.0.0.1', 8000))
 
-while True:
-    welcome_msg = s.recv(1024)
+try:
+    s.connect((IP, PORT))
+except ConnectionRefusedError:
+    print("This server has not started yet...")
+    sys.exit()
+
+
+def contact_server():
+    encoded_usern = usern.encode("utf-8")
+    s.send(encoded_usern)
+
+    welcome_msg = s.recv(MSG_SIZE)
     welcome_msg = welcome_msg.decode("utf-8")
-    print(welcome_msg)
+    if "ERROR" in welcome_msg:
+        print(welcome_msg)
+        sys.exit()
+    else:
+        print(welcome_msg)
+
+
+def listen_for_messages():
+    while True:
+        msg = s.recv(MSG_SIZE)
+        msg = msg.decode("utf-8")
+        print(msg)
+
+
+def send_messages():
+    while True:
+        msg = input(">>")
+        msg = f"{usern}: {msg}"
+        msg = msg.encode("utf-8")
+        s.send(msg)
+
+
+def main():
+    contact_server()
+    listen_for_messages_thread = threading.Thread(target=listen_for_messages)
+    send_messages_thread = threading.Thread(target=send_messages)
+
+    listen_for_messages_thread.start()
+    send_messages_thread.start()
+
+
+if __name__ == "__main__":
+    main()
--- server.py
+++ server.py
@@ -1,14 +1,50 @@
 import socket
+import threading
+
+IP = '127.0.0.1'
+PORT = 8000
+MSG_SIZE = 2048
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((socket.gethostname(), 8000))
+s.bind((IP, PORT))
 s.listen(10)
 print("Listening for connections...")
 
-clients = []
+clients = {}
 
-while True:
-    clientsocket, address = s.accept()
-    print(f"Connection from {address} has been established!")
-    clientsocket.send(bytes("You have joined MysteryCoder456 Chatroom.", "utf-8"))
-    clients.append(clientsocket)
+def new_client():
+    while True:
+        clientsocket, address = s.accept()
+
+        client_usern = clientsocket.recv(MSG_SIZE)
+        client_usern = client_usern.decode("utf-8")
+
+        if client_usern in clients.keys():
+            clientsocket.send(bytes("ERROR: Username already taken!", "utf-8"))
+        else:
+            clients[client_usern] = clientsocket
+
+            print(f"Connection from {address} has been established! Username = {client_usern}")
+            clientsocket.send(bytes("You have joined MysteryCoder456 Chatroom.", "utf-8"))
+            listen_for_messages_thread = threading.Thread(
+                target=listen_for_messages,
+                args=(clients[client_usern],)
+            )
+            listen_for_messages_thread.start()
+
+
+def listen_for_messages(cs: socket.socket):
+    while True:
+        if len(clients) > 0:
+            msg = cs.recv(MSG_SIZE)
+            msg = msg.decode("utf-8")
+            print(msg)
+
+
+def main():
+    new_clients_thread = threading.Thread(target=new_client)
+    new_clients_thread.start()
+
+
+if __name__ == "__main__":
+    main()
